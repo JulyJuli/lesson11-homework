438f456900aba91cc032488735b4659c924ad814
Anna <frolenkova.a.a@gmail.com>
2020-04-05 11:54:24 +0300

    game

---
+++ GameWithBasket/Gamer's classes/NotePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameWithBasket
+{
+    public class NotePlayer :Player
+    {
+        private readonly List<int> notes ;
+        public NotePlayer ()
+        {
+            notes = new List<int>();
+        }
+        internal override string TypeName { get => "NotePlayer"; }
+        public override int GetValue(List<int> trials)
+        {
+            int randValue = new Random().Next(minValue, maxValue);
+            var counter = 1;
+
+            while (notes.Contains(randValue))
+            {
+                if (counter<6)
+                {
+                    randValue = new Random().Next(minValue, maxValue);
+                    counter++;
+                }
+               
+            }
+            
+            notes.Add(randValue);
+            return randValue;
+        }
+    }
+}
---
+++ GameWithBasket/Gamer's classes/UberPlayer.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+namespace GameWithBasket
+{
+    public class UberPlayer : Player
+    {
+        int startValue = minValue;
+        internal override string TypeName { get => "UberPlayer"; }
+
+        public override int GetValue(List<int> trials)
+        {  
+            return startValue++;
+        }
+    }
+}
