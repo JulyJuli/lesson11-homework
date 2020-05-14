ceaa7941dd4daee3d7cd8c80380264d715c8916e
Oleksii <alexmkh89@gmail.com>
2020-05-14 23:52:53 +0300

    DeleagateHolder class was removed

--- Basket_Lib/DelegateHolder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Basket_Lib
-{
-    // Need to be reworked with Action<>.
-    public class DelegateHolder
-    {
-        public delegate void ShowMessage(string str);
-    }
-}
--- Basket_Lib/Game.cs
+++ Basket_Lib/Game.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
-using static Basket_Lib.DelegateHolder;
 
 namespace Basket_Lib
 {
